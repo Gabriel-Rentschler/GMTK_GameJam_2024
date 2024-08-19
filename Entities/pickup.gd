91e8bacb2879a63fa100697a09c2b7d277f45d01
Gabriel Rentschler de Araujo <gabrielrdearaujo1023@hotmail.com>
2024-08-19 13:25:02 -0300

    object floating animation

--- Entities/pickup.gd
+++ Entities/pickup.gd
@@ -1,6 +1,7 @@
 extends RayCast3D
 
 var obj: RigidBody3D
+var anim: AnimationPlayer
 var collider: RigidBody3D
 @onready var point = %"Hold"
 
@@ -16,12 +17,15 @@ func _process(delta: float) -> void:
 		
 	if Input.is_action_just_pressed("carry") && obj != null:
 		var direction : Vector3 = obj.global_transform.origin.direction_to(point.global_transform.origin)
+		anim = obj.get_node("AnimationPlayer")
+		anim.pause()
 		obj.apply_central_impulse(-global_transform.basis.z * 60)
 		obj = null
 		collider = null
 	elif obj == null && collider != null && (!collider.get_groups().find("grab") && collider.is_class("RigidBody3D")) && Input.is_action_just_pressed("carry"):
 		obj = collider
-		
+		anim = obj.get_node("AnimationPlayer")
+		anim.play("floating")
 	if obj:
 		obj.position = point.global_position
 		obj.linear_velocity = obj.linear_velocity * 0.8
